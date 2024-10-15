8e46160b0b8e111c35a7f0367dc642833be9a360
Audrey <audrey.bonis@fuzeo.fr>
2024-10-15 11:06:08 +0200

    feat: db connect

--- src/main/java/oc/chatopbackend/ChatopApplication.java
+++ src/main/java/oc/chatopbackend/ChatopApplication.java
@@ -1,13 +1,28 @@
 package oc.chatopbackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
 public class ChatopApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(ChatopApplication.class);
+
+
     public static void main(String[] args) {
+        logger.info("Tentative de démarrage de l'application...");
         SpringApplication.run(ChatopApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logOnStartup() {
+        logger.info("Connexion à la base de données réussie !");
+        logger.info("Application démarrée avec succès !");
     }
 
 }
